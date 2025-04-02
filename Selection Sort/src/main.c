257405b275d62fbd2052aa84faedfc517106a3e4
Husen00 <hmhusen1998@gmail.com>
2025-04-02 13:05:42 +0200

    Implement selectionSort function

--- Selection Sort/src/main.c
+++ Selection Sort/src/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+
+
+void selectionSort(int A[], int n) {
+    for (int j = 0; j< n - 1; j++) {
+      int min = j;
+      for (int i = j + 1; i < n; i++) {
+  
+        if (A[i] < A[min])
+          min = i;
+      }
+  
+      swap(&A[min], &A[j]);
+    }
+  }
+
 int main(int argc, char const *argv[])
 {
     printf("Selection Sort\n");
