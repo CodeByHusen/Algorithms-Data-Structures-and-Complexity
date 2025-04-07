e18e95fecc74bbd9387b493808f43cf44fcad902
Husen00 <hmhusen1998@gmail.com>
2025-04-07 12:08:15 +0200

    create Insertion Sort funktion

--- Insertion Sort/src/main.c
+++ Insertion Sort/src/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+
+void insertion_sort(int* arr, int n)
+{
+	int i, j, k;
+	for(i = 1; i<n; ++i){
+		int temp = arr[i];
+
+		for(j = i; j > 0 && arr[j -1] > temp; --j)
+		swap(&arr[j - 1], &arr[j]);
+
+	}
+}
+
+
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!\n");
+    
 
     return 0;
 }
