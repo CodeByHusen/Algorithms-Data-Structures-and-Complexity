756427ff548fab7e65ed8af40a0038a22f50735d
Husen00 <hmhusen1998@gmail.com>
2025-04-14 13:17:02 +0200

    implement the main function

--- Comparison of sorting methods/src/main.c
+++ Comparison of sorting methods/src/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-int main(int argc, char const *argv[])
+int A[10000];
+
+
+void swap(int *xp, int *yp)
 {
-    printf("Comparison of sorting methods!\n");
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
 
+
+void bubbleSort(int A[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)     
+ 
+       
+       for (j = 0; j < n-i-1; j++)
+           if (A[j] > A[j+1])
+              swap(&A[j], &A[j+1]);
+}
+
+int main()
+{
+    printf("CLOCKS_PER_SEC=%d\n", CLOCKS_PER_SEC );
+    for( int n=1000; n<=10000; n+=1000 ) {
+        for( int i=0; i<n; ++i ) {
+            A[i] = rand();
+        }
+        clock_t tick = clock();
+        bubbleSort( A, n );
+        clock_t tock = clock();
+        printf("%8d %8d\n", n, tock-tick );
+    }
     return 0;
 }
