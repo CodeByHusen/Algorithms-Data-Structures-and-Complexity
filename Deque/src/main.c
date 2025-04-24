5ba10dc39716b9fa50db39ef210fb465a7a5d6dc
Husen00 <hmhusen1998@gmail.com>
2025-04-24 14:34:38 +0200

    Function declarations

--- Deque/src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("Create Deque project\n");
-
-    return 0;
-}
---
+++ Deque/src/main.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <deque>
+
+
+
+void push_front(int x);   // head
+int pop_front();          // head
+void push_back(int x);    // tail
+int pop_back();           // tail
+
+int empty(); //returns 1 for true and 0 for false
+int full(); // returns 1 for true and 0 for false
+
+
+
+
+int main()
+{
+
+
+    return 0;
+}
