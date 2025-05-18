ab4dda02fc2ce201633f93867e0e83f7a018006d
Husen00 <hmhusen1998@gmail.com>
2025-05-18 13:57:23 +0200

    implement a function that reads a line from standard input (e.g. keyboard) and creates a linked list from it.

--- Merge two lists into one/Merge two lists into one/src/main.c
+++ Merge two lists into one/Merge two lists into one/src/main.c
@@ -44,6 +44,35 @@ void print_List(struct List* L)  // prints all list elements in one line like
 	printf("]\n");
 }
 
+struct List* read_list()
+{
+    char line[256];
+    fgets( line, sizeof(line), stdin);
+    struct List* L = NULL;
+    struct List* last = NULL;
+    char* number = strtok(line, " \n");
+    if(number==NULL) {
+        return L;
+    }
+    int key = atoi(number);
+    while(key != 0) {
+        struct List* p = new_List(key);
+        if(L == NULL) {
+            L = p;
+        } else {
+            last->next = p;
+        }
+        last = p;
+        number = strtok(NULL, " \n");
+        if(number==NULL) {
+            return L;
+        }
+        key = atoi(number);
+    }
+    return L;
+
+}
+
 int main(int argc, char const *argv[])
 {
     
