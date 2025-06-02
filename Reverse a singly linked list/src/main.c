477028b63bd6ce534b5754d3680ef4d04b7aae5a
Husen00 <hmhusen1998@gmail.com>
2025-06-02 16:25:28 +0200

    ctrate a list for Definition of the structure for a node in a singly linked list

--- Reverse a singly linked list/src/main.c
+++ Reverse a singly linked list/src/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+
+// Definition of the structure for a node in a singly linked list
+struct List {
+    int key;              // The value stored in the node
+    struct List* next;    // Pointer to the next node
+};
+
+
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!\n");
+    printf("Reverse a singly linked list\n");
 
     return 0;
 }
